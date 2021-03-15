65cfa1aadde0d524fcee45641acc095c0fb01fe7
Syuichi Murashima <mura-git@vwnet.jp>
2021-03-15 09:26:06 +0900

    1st commit

---
+++ ReadMe.txt
@@ -0,0 +1,22 @@
+﻿■ これは何?
+リモートコンピューター証明書を拇印で指定し、期限が切れていたら削除します これは何?
+
+■ 使い方
+PowerShell プロンプトで以下コマンド入力すると、今月の Windows Update 日を返します
+
+RemoveCert リモートコンピューター名 証明書の拇印
+
+■ セットの仕方
+PowerShell プロンプトで install.ps1 を実行してください
+
+■ Uninstall 方法
+uninstall.ps1 を実行して下さい
+(問い合わせが来たら Enter)
+
+■ 動作確認環境
+On Windows
+	5.1
+
+■ リポジトリ
+https://github.com/MuraAtVwnet/RemoveCert
+git@github.com:MuraAtVwnet/RemoveCert.git
---
+++ RemoveCert.psm1
@@ -0,0 +1,22 @@
+﻿###############################################
+# 期限の切れた証明書を削除する
+###############################################
+function RemoveExpiredCertificate($ThumbprintString){
+    $ThumbprintOrg = $ThumbprintString
+    $Thumbprint = $ThumbprintOrg -replace " ",""
+    $Certs = dir Cert:\LocalMachine\my
+    [array]$Tergets = $Certs |? Thumbprint -eq $Thumbprint
+    $Tergets | Select PSParentPath, Issuer, NotAfter
+    foreach( $Terget in $Tergets ){
+        if( $Terget.NotAfter -lt (Get-Date) ){
+            $CertPath = Join-path $Terget.PSParentPath $Terget.PSChildName
+            echo "Expired certificate : $CertPath"
+            Remove-Item $CertPath
+        }
+    }
+}
+
+
+function RemoveCert([String]$ComputerName, [String]$ThumbprintString){
+    Invoke-Command -ScriptBlock $function:RemoveExpiredCertificate -ComputerName $ComputerName -ArgumentList $ThumbprintString
+}
