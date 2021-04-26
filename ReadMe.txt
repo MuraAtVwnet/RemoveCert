2da6707ed0c1440415c1c7e4e93fcd5eaa51b0c6
Syuichi Murashima <mura-github@vwnet.jp>
2021-04-27 08:21:03 +0900

    証明書を削除しない「チェックのみ」をサポート

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,21 +1,28 @@
 ﻿■ これは何?
 リモートコンピューター証明書を拇印で指定し、期限が切れていたら削除します これは何?
 
+
 ■ 使い方
-PowerShell プロンプトで以下コマンド入力すると、今月の Windows Update 日を返します
+PowerShell プロンプトで以下コマンド入力すると、証明書の期限を確認し、期限が切れていたら削除をします
 
 RemoveCert リモートコンピューター名 証明書の拇印
 
-■ セットの仕方
+削除はせずに、証明書の確認だけをする場合は、CheckCert を使います
+
+CheckCert リモートコンピューター名 証明書の拇印
+
+
+■ インストールの仕方
 PowerShell プロンプトで install.ps1 を実行してください
+
 
 ■ Uninstall 方法
 uninstall.ps1 を実行して下さい
 (問い合わせが来たら Enter)
 
 ■ 動作確認環境
-On Windows
-	5.1
+Windows PowerShell 5.1 On Windows
+
 
 ■ リポジトリ
 https://github.com/MuraAtVwnet/RemoveCert
--- RemoveCert.psm1
+++ RemoveCert.psm1
@@ -1,22 +1,29 @@
 ﻿###############################################
 # 期限の切れた証明書を削除する
 ###############################################
-function RemoveExpiredCertificate($ThumbprintString){
-    $ThumbprintOrg = $ThumbprintString
-    $Thumbprint = $ThumbprintOrg -replace " ",""
-    $Certs = dir Cert:\LocalMachine\my
-    [array]$Tergets = $Certs |? Thumbprint -eq $Thumbprint
-    $Tergets | Select PSParentPath, Issuer, NotAfter
-    foreach( $Terget in $Tergets ){
-        if( $Terget.NotAfter -lt (Get-Date) ){
-            $CertPath = Join-path $Terget.PSParentPath $Terget.PSChildName
-            echo "Expired certificate : $CertPath"
-            Remove-Item $CertPath
-        }
-    }
+function RemoveExpiredCertificate([string]$ThumbprintString, [switch]$CheckOnly){
+	$ThumbprintOrg = $ThumbprintString
+	$Thumbprint = $ThumbprintOrg -replace " ",""
+	$Certs = dir Cert:\LocalMachine\my
+	[array]$Tergets = $Certs |? Thumbprint -eq $Thumbprint
+	$Tergets | Select PSParentPath, Issuer, NotAfter
+	if( -not $CheckOnly ){
+		foreach( $Terget in $Tergets ){
+			if( $Terget.NotAfter -lt (Get-Date) ){
+				$CertPath = Join-path $Terget.PSParentPath $Terget.PSChildName
+				echo "Expired certificate : $CertPath"
+				Remove-Item $CertPath
+			}
+		}
+	}
 }
 
 
 function RemoveCert([String]$ComputerName, [String]$ThumbprintString){
-    Invoke-Command -ScriptBlock $function:RemoveExpiredCertificate -ComputerName $ComputerName -ArgumentList $ThumbprintString
+	Invoke-Command -ScriptBlock $function:RemoveExpiredCertificate -ComputerName $ComputerName -ArgumentList $ThumbprintString
+}
+
+
+function CheckCert([String]$ComputerName, [String]$ThumbprintString){
+	Invoke-Command -ScriptBlock $function:RemoveExpiredCertificate -ComputerName $ComputerName -ArgumentList $ThumbprintString, "-CheckOnly"
 }
